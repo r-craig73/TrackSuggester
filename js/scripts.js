98d841b417c8b7e00123803da17753147eecc00a
r-craig73 <ron.craig@comcast.net>
2018-01-12 14:12:44 -0800

    add debugging tools to read form values

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
-  var myName = $("#name1").val();
-  var myApplication = $("#favoriteApp").val();
+  alert("Beginning");
+
+  $("#submit").click(function(event) {
+  var myName = $("input#name1").val();
+  var myApp = $("#favoriteApp").val();
   var myCompany = $("input:radio[name=companySize]:checked").val();
   var myOperatingSystem = $("input:radio[name=operatingSystem]:checked").val();
 
-  $("#submit").click(function() {
+  $(".name1").text(myName);
+  $("#favoriteApp").text(myApp);
+  $("input:radio[name=companySize]:checked").text(myCompany);
+  $("input:radio[name=operatingSystem]:checked").text(myOperatingSystem);
   $("#suggestions").fadeIn();
+  alert(myName);
+  alert(myApp);
+  alert(myCompany);
+  alert(myOperatingSystem);
+  event.preventDefault();
+  alert("End");
   });
+
 
 });
